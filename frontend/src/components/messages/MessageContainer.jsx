2a29bbc0d40ac2f8d0fe1b2b4d889cbb86fc15b7
rabnawaz-aptech <rabnawaz.aptech@gmail.com>
2024-03-06 01:19:22 +0500

    Updated

--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -1,18 +1,28 @@
 // import Message from "./Message";
 // import MessageInput from "./MessageInput";
+import { useEffect } from "react";
+import useConversation from "../../store/useConversation";
 import MessageInput from "./MessageInput";
 import Messages from "./Messages";
 import { TiMessages } from "react-icons/ti";
 
 const MessageContainer = () => {
-    const noChatSelected = true;
+    const {selectedConversation, setSelectedConversation} = useConversation();
+
+    useEffect(() => {
+        // cleanup function (unmounts)
+        return () => {
+            setSelectedConversation(null);
+        }
+    }, []);
+    
 	return (
 		<div className='md:min-w-[450px] flex flex-col'>
-			{noChatSelected ? (<NoChatSelected/>) : (
+			{!selectedConversation ? (<NoChatSelected/>) : (
                 <>
 				{/* Header */}
 				<div className='bg-slate-500 px-4 py-2 mb-2'>
-					<span className='label-text'>To:</span> <span className='text-gray-900 font-bold'>John doe</span>
+					<span className='label-text'>To:</span> <span className='text-gray-900 font-bold'>{selectedConversation.fullName}</span>
 				</div>
 
 				<Messages />
