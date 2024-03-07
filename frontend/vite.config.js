829554bedfc51fa0a4e8ec4e21da6e6d1f8450f1
rabnawaz-aptech <rabnawaz.aptech@gmail.com>
2024-03-07 22:28:04 +0500

    Updated

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,11 +6,11 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 5000,
-    proxy: {
-      "/api": {
-        target: "https://chat-n9i7wuruo-rabnawazs-projects.vercel.app ",
-        changeOrigin: true,
-      },
-    },
+    // proxy: {
+    //   "/api": {
+    //     target: "https://chat-n9i7wuruo-rabnawazs-projects.vercel.app ",
+    //     changeOrigin: true,
+    //   },
+    // },
   },
 });
