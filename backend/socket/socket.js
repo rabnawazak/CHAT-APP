bfff83228e733bade200fac6f2c52c67abb8616f
Rabnawaz <140235428+rabnawazak@users.noreply.github.com>
2025-09-09 23:42:14 +0500

    Update socket.js

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -8,7 +8,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000", // change if frontend served from different origin
+    origin: "http://localhost:5000", // change if frontend served from different origin
     methods: ["GET", "POST"],
   },
 });
